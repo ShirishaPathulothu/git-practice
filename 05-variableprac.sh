54f4eb1758d40620b459480093e0c3441fad44bd
Shirishapathulothu <pathulothushirisha@gmail.com>
2025-01-04 13:59:41 +0530

    practice file

---
+++ 05-variableprac.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Hi $PERSON2, how are you ?"
+echo "$PERSON2:: Hi $PERSON1, i am good how about you."
+echo "$PERSON1:: I am doing fine, how your work is going."
+echo "$PERSON2:: Its Going good.
