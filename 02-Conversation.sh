3953dca0d73c51b0850ce0945eb6fe66911360b8
Shirishapathulothu <pathulothushirisha@gmail.com>
2025-01-04 14:21:53 +0530

    updated

--- 02-Conversation.sh
+++ 02-Conversation.sh
@@ -3,5 +3,4 @@
 echo "Rakesh:: Hi Ravi, how are you ?"
 echo "Ravi:: Hi Rakesh, i am good how about you."
 echo "Rakesh:: I am doing fine, how your work is going."
-echo "Ravi:: Its Going good.
-
+echo "Ravi:: Its Going good."
--- 03-variables.sh
+++ 03-variables.sh
@@ -5,4 +5,5 @@ PERSON2=Ravi
 echo "$PERSON1:: Hi $PERSON2, how are you ?"
 echo "$PERSON2:: Hi $PERSON1, i am good how about you."
 echo "$PERSON1:: I am doing fine, how your work is going."
-echo "$PERSON2:: Its Going good.
+echo "$PERSON2:: Its Going good."
+
