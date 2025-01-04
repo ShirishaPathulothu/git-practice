41760e3e52614dcf2cc7295cadf3b1354dac70cb
Shirishapathulothu <pathulothushirisha@gmail.com>
2025-01-04 15:25:31 +0530

    learining shell

--- 07-array.sh
+++ 07-array.sh
@@ -2,11 +2,12 @@
 
 NAMES=("Baby" "Lucky" "Rani")
 
-echo "First name is: ${NAME[0]}"
-echo "Second name is: ${NAME[1]}"
-echo "Third name is: ${NAME[2]}"
+echo "First name is: ${NAMES[0]}"
+echo "Second name is: ${NAMES[1]}"
+echo "Third name is: ${NAMES[2]}"
 
 # index starts from 0, 
 # to print all names at one time is @
 
-echo "Entered names are: ${NAME[@]}"
+echo "Entered names are: ${NAMES[@]}"
+
