4a3792b18524a8146773a651eac0509877789d15
Shirishapathulothu <pathulothushirisha@gmail.com>
2025-01-04 12:15:27 +0530

    variables

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=Rakesh
+PERSON2=Ravi
+
+echo "$PERSON1:: Hi $PERSON2, how are you ?"
+echo "$PERSON2:: Hi $PERSON1, i am good how about you."
+echo "$PERSON1:: I am doing fine, how your work is going."
+echo "$PERSON2:: Its Going good.
--- Conversation.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Rakesh:: Hi Ravi, how are you ?"
-echo "Ravi:: Hi Rakesh, i am good how about you."
-echo "Rakesh:: I am doing fine, how your work is going."
-echo "Ravi:: Its Going good.
-
