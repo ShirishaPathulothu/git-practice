6f8fd36c5c693c396a5433df31302e857c7a714f
Shirishapathulothu <pathulothushirisha@gmail.com>
2025-01-19 11:30:20 +0530

    shell

---
+++ 08-deletelogfiles.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+SOURCE_DIR=/home/ec2-user/logs
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+if [ -d $SOURCE_DIR ]
+then
+   echo -e "$SOURCE_DIR is $G Exists $N"
+else
+   echo -e "$SOURCE_DIR is $R does not Exists $N"
+fi
+
+FILES=$(find $SOURCE_DIR -name "*.log" -mtime +14)
+echo "files: $FILES"
+
+while IFS= read -r file
+do
+   echo "deleting file: $FILE"
+   rm -rf $FILE
+done <<< $FILES   
