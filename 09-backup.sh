aca3a436ee802a6d184bd78ee35e85c7fd152479
Shirishapathulothu <pathulothushirisha@gmail.com>
2025-01-21 08:37:10 +0530

    shell

---
+++ 09-backup.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-14}
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+
+USAGE(){
+    echo "USAGE:: sh 09-backup.sh <source> <destination> <date(optional)>"
+}
+
+if [ $# -lt 2 ]
+then
+    USAGE
+    exit 1
+fi
+
+if [ -d $SOURCE_DIR ]
+then
+    echo "$SOURCE_DIR does not Exists..please check"
+fi
+
+if [ ! -d $DEST_DIR ]
+then
+    echo "$DEST_DIR does not Exists..please check"
+fi
+
+echo "files: $FILES"
+
+if [ ! -Z $FILES ]
+then
+    echo "files are found"
+    ZIP_FILE="$DEST_DIR/app_logs-$TIMESTAMP.zip"
+    find $SOURCE_DIR -name "*.log" -mtime +$DAYS | zip "$ZIP_FILE" -@
+    if [ -f $ZIP_FILE ]
+    then
+        echo "Successfully zipped files older then $DAYS"
+        while IFS= read -r file
+        do
+           echo "Deleting file: $file"
+           rm -rf $file
+        done <<< $FILES
+    else
+        echo "Zipping files failed"
+        exit 1
+    fi
+else
+    echo "No files found older than $DAYS"
+fi                   
